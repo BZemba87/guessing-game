f4d26f267346ada80019fa5d740e2b55b42277d3
BZemba87 <zembabernadette@gmail.com>
2022-06-05 17:58:59 +0000

    Add string with player name and tell player computer is thinking of number and asks player to guess

--- run.py
+++ run.py
@@ -2,3 +2,5 @@ import random
 number = random.randint(1, 100)
 
 player_name = input("Ready to play?  Please enter your name: \n")
+number_of_guesses = 0
+print(''+ player_name+ ', I am thinking of a number between 1 and 100, can you guess what it is?')
