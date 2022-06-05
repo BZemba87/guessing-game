e8dc5694580f6306ac0461543f973c079422e24b
BZemba87 <zembabernadette@gmail.com>
2022-06-05 18:58:16 +0000

    Add while loop

--- run.py
+++ run.py
@@ -4,3 +4,13 @@ number = random.randint(1, 100)
 player_name = input("Ready to play?  Please enter your name: \n")
 number_of_guesses = 0
 print(''+ player_name+ ', I am thinking of a number between 1 and 100, can you guess what it is?')
+
+while number_of_guesses < 5:
+    guess = int(input())
+    number_of_guesses += 1
+    if guess < number:
+        print('Your guess is too low!')
+    if guess > number:
+        print('Your guess is too high!')
+    if guess == number:
+        break
