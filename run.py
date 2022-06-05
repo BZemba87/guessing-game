29e42c2f82035ef42ed2cc21b732e9efc636c49b
BZemba87 <zembabernadette@gmail.com>
2022-06-05 19:35:55 +0000

    Add if/else statement to check if user input is correct or not

--- run.py
+++ run.py
@@ -1,16 +1,21 @@
 import random 
-number = random.randint(1, 100)
+number = random.randint(1, 10)
 
 player_name = input("Ready to play?  Please enter your name: \n")
 number_of_guesses = 0
-print(''+ player_name+ ', I am thinking of a number between 1 and 100, can you guess what it is?')
+print(''+ player_name+ ', I am thinking of a number between 1 and 10, can you guess what it is?')
 
 while number_of_guesses < 5:
-    guess = int(input())
-    number_of_guesses += 1
-    if guess < number:
-        print('Your guess is too low!')
-    if guess > number:
-        print('Your guess is too high!')
-    if guess == number:
-        break
+        guess = int(input())
+        number_of_guesses += 1
+        if guess < number:
+            print('Your guess is too low!')
+        if guess > number:
+            print('Your guess is too high!')
+        if guess == number:
+            break
+
+if guess == number:
+    print('You guessed the number in '+ str(number_of_guesses) + ' tries!')
+else:
+    print('You did not guess the number, the number was '+str(number))
