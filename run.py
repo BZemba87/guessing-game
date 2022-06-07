c07721fb7e4e6e772bb7c31c13dd9b34d373c970
BZemba87 <zembabernadette@gmail.com>
2022-06-07 21:40:59 +0000

    Add comments to code

--- run.py
+++ run.py
@@ -1,21 +1,32 @@
 import random 
-number = random.randint(1, 10)
+
+print('Welcome to the Number Guessing Game!')
+
+number = random.randint(1, 10) #random number generated between 1 and 10
 
 player_name = input("Ready to play?  Please enter your name: \n")
-number_of_guesses = 0
+guess = None #players current guess
+number_of_guesses = 0 #keeps track of players current guess
+guessed = False #keeps track of whether or not the player has guessed the number
+
 print(''+ player_name+ ', I am thinking of a number between 1 and 10, can you guess what it is?')
 
-while number_of_guesses < 5:
-        guess = int(input())
-        number_of_guesses += 1
+while(not guessed):
+    guess = input()
+    number_of_guesses += 1 #increments the number of guesses 
+    if guess.isdigit(): #check  player input is a number
+        guess = int(guess) #converting guess into integer value
+       
         if guess < number:
-            print('Your guess is too low!')
-        if guess > number:
-            print('Your guess is too high!')
-        if guess == number:
-            break
+            print('Your guess is too low!') #hints to player that the number is higher
+        elif guess > number:
+            print('Your guess is too high!') #hints to player that the number is lower
+        
+        else:
+            guessed = True #player has guessed correct number
+    else:
+        print('Must enter a number!  Try again.') #executes when player entry is invalid ie not a number
 
-if guess == number:
-    print('You guessed the number in '+ str(number_of_guesses) + ' tries!')
-else:
-    print('You did not guess the number, the number was '+str(number))
+
+print('Yay, you got it!  You guessed it in '+ str(number_of_guesses) + ' tries!') #executes when player guesses correct number
+
