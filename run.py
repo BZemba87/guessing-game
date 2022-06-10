398a12de4cccb0fbdb8886c7c2e5e587dca0a5ff
BZemba87 <zembabernadette@gmail.com>
2022-06-10 12:43:05 +0000

    Add guess limit of 5

--- run.py
+++ run.py
@@ -11,22 +11,28 @@ guessed = False #keeps track of whether or not the player has guessed the number
 
 print(''+ player_name+ ', I am thinking of a number between 1 and 10, can you guess what it is?')
 
+
 while(not guessed):
     guess = input()
-    number_of_guesses += 1 #increments the number of guesses 
-    if guess.isdigit(): #check  player input is a number
+    number_of_guesses += 1 #increments the number of guesses    
+    if number_of_guesses == 5: #number of guesses limited to 5  
+        print('You have run out of guesses!') #executes if player reaches 5 guesses
+
+    if guess.isdigit(): #checks player input is a number
         guess = int(guess) #converting guess into integer value
-       
+        
+
+    
         if guess < number:
             print('Your guess is too low!') #hints to player that the number is higher
         elif guess > number:
-            print('Your guess is too high!') #hints to player that the number is lower
-        
+            print('Your guess is too high!') #hints to player that the number is lower 
         else:
             guessed = True #player has guessed correct number
+    
     else:
         print('Must enter a number!  Try again.') #executes when player entry is invalid ie not a number
 
-
 print('Yay, you got it!  You guessed it in '+ str(number_of_guesses) + ' tries!') #executes when player guesses correct number
+
 
