261cca8a8f93566606dce6ecb32fa134fc631cf7
BZemba87 <zembabernadette@gmail.com>
2022-06-10 14:47:17 +0000

    fix all errors that came up on pep8 validator

--- run.py
+++ run.py
@@ -1,39 +1,34 @@
-import random 
+import random
 
 print('Welcome to the Number Guessing Game!')
 
-number = random.randint(1, 10) #random number generated between 1 and 10
+number = random.randint(1, 10)  # random number generated between 1 and 10
 
 player_name = input("Ready to play?  Please enter your name: \n")
-guess = None #players current guess
-number_of_guesses = 0 #keeps track of players current guess
-guessed = False #keeps track of whether or not the player has guessed the number
+guess = None  # players current guess
+number_of_guesses = 0  # keeps track of players current guess
+guessed = False  # tracks whether or not the player has guessed right
 
-print(''+ player_name+ ', I am thinking of a number between 1 and 10, can you guess what it is?')
+print('' + player_name + ', guess a number between 1 and 10!')
 
 while(not guessed):
     guess = input()
-    number_of_guesses += 1 #increments the number of guesses    
+    number_of_guesses += 1  # increments the number of guesses
     
-    if guess.isdigit(): #checks player input is a number
-        guess = int(guess) #converting guess into integer value
+    if guess.isdigit():  # checks player input is valid (integer)
+        guess = int(guess)  # converting guess into integer value
 
-        if number_of_guesses == 5: #limits number of player guesses to 5
+        if number_of_guesses == 5:  # limits number of player guesses to 5
             print('You have run out of guesses!')
-            break #stops printing hints when guesses run out 
+            break  # stops printing hints when guesses run out
         
         if guess < number:
-            print('Your guess is too low!') #hints to player that the number is higher
+            print('Your guess is too low!')  # hints to player to guess higher
         elif guess > number:
-            print('Your guess is too high!') #hints to player that the number is lower 
+            print('Your guess is too high!')  # hints to player to guess lower
         else:
-            guessed = True #player has guessed correct number
-            
+            guessed = True  # player has guessed correct number
     else:
-        print('Must enter a number!  Try again.') #executes when player entry is invalid ie not a number
-      
+        print('Must enter a number!  Try again.')  # tells player input invalid
 else:
-    print('Yay, you got it!  You guessed it in '+ str(number_of_guesses) + ' tries!') #executes when player guesses correct number
-
-
-    
+    print('YAY!  You guessed it in ' + str(number_of_guesses) + ' tries!')
